b1aae55424559295df312ad7879e9d8a33e0ee1f
prabhashk123 <sunriseprabhash.bh@gmail.com>
2024-02-11 01:00:51 +0530

    fixed multiprocessing

--- Pythons/pythonaBasic/multiprocessings.py
+++ Pythons/pythonaBasic/multiprocessings.py
@@ -10,26 +10,35 @@ import multiprocessing
 # from multiprocessing import Process
 # from multiprocessing import Process
 import requests
+import concurrent.futures
 
 def downloadFile(url,name):
     print(f"Start downloading{name}")
     response=requests.get(url)
-    open(f"files//file{name}.jpg","wb").write(response.content)
+    open(f"file {name}.jpg","wb").write(response.content)
     print(f"finished downloading{name}")
+
 if __name__ == '__main__':
-    url="https://picsum.photos/200"
+    url="http://127.0.0.1:8000/product_imgs"
     # for multiprocessing
     pros=[]
 # use underscore here for throw because i dont need i print() so use.
 # for _ in range(5):
-
-    for i in range(5):
-        # downloadFile(url,i)
-        # use multiprocessing
-        p=multiprocessing.Process(target=downloadFile,args=[url,i])
-        p.start()
-        pros.append(p)
-    for p in pros:
-        p.join()
+    
+    # for i in range(5):
+    #     # downloadFile(url,i)
+    #     # use multiprocessing
+    #     p=multiprocessing.Process(target=downloadFile,args=[url,i])
+    #     p.start()
+    #     pros.append(p)
+    # for p in pros:
+    #     p.join()
+# for module con
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        l1=[url for i in range(60)]
+        l2=[i for i in range(60)]
+        results=executor.map(downloadFile,l1,l2)
+        for r in results:
+            print(r)
 
 
