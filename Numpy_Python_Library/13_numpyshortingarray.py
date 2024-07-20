108b967f035bbfd8f96148bfd1ea22b5a7412d43
prabhashk123 <sunriseprabhash.bh@gmail.com>
2024-07-20 11:12:51 +0530

    added sorting in numpy

---
+++ Numpy_Python_Library/13_numpyshortingarray.py
@@ -0,0 +1,23 @@
+# sort()- the numpy ndarray object has a function which is called sort(). and this will sort a specified array.
+"""Sorting the 1-D array"""
+import numpy as np 
+pb=np.array([3,2,0,1])
+# by default shorting asendingorder pahle chhota fir bara.
+print(np.sort(pb)) #This method is like as copy().
+
+# sort the array alphbetically first a then b then c
+import numpy as np
+pb=np.array(['banana','cherry','apple'])
+print(np.sort(pb))
+
+# sort the boolean array:-
+import numpy as np
+pb=np.array([True, False, True])
+print(np.sort(pb))
+
+"""Sorting the 2-D & 3-D array"""
+import numpy as np
+pb=np.array([[3,2,4],[5,0,1]])
+print(np.sort(pb))
+
+
