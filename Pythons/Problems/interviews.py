9080b0a35b6e2cb1d8c47cedbc22638555b2fab3
prabhashk123 <sunriseprabhash.bh@gmail.com>
2024-08-05 12:55:45 +0530

    added factorial number

--- Pythons/Problems/interviews.py
+++ Pythons/Problems/interviews.py
@@ -454,6 +454,19 @@ else:
     print(f"{num} is not a prime number. ")
 
 # Write a program to find outy the factorial of the number
+# check if the number is negative, positive or zero
+def compute_factorial(num):
+    factorial = 1
+    if num < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+    elif num == 0:
+        print("The factorial of 0 is 1")
+    else:
+        for i in range(1,num+1):
+            factorial = factorial*i
+        print("The factorial of",num,"is",factorial)
+# take input from the user
+compute_factorial(int(input("Enter a number")))
 
     
 ## Write return type function and Returning Different Types
